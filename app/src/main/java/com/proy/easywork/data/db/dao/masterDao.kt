99d645c080cb696a38ab6ad90f51b7ba426ffb7b
Joaquin-Carbajal <49212899+Joaquin-Carbajal@users.noreply.github.com>
2022-05-19 18:23:02 -0500

    Modificaciones

--- app/src/main/java/com/proy/easywork/data/db/dao/masterDao.kt
+++ app/src/main/java/com/proy/easywork/data/db/dao/masterDao.kt
@@ -4,17 +4,50 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.proy.easywork.data.db.entity.CategoriaServicio
+import com.proy.easywork.data.db.entity.*
 
 @Dao
 interface masterDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addCategoriaServicio(categoriaServicio: CategoriaServicio): Long
-
+    fun addCategoriaServicio(categoriaServicio: CategoriaServicio): Long
     @Query("DELETE FROM categoria_servicio_table")
     fun deleteCategoria()
-
     @Query("SELECT * FROM categoria_servicio_table")
     fun listCategoria(): List<CategoriaServicio?>?
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addDistrito(distrito: Distrito): Long
+    @Query("DELETE FROM distrito_table")
+    fun deleteDistrito()
+    @Query("SELECT * FROM distrito_table")
+    fun listDistrito(): List<Distrito?>?
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addMedioPago(medioPago: MedioPago): Long
+    @Query("DELETE FROM medio_pago_table")
+    fun deleteMedioPago()
+    @Query("SELECT * FROM medio_pago_table")
+    fun listMedioPago(): List<MedioPago?>?
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addTipoBusqueda(tipoBusqueda: TipoBusqueda): Long
+    @Query("DELETE FROM tipo_busqueda_table")
+    fun deleteTipoBusqueda()
+    @Query("SELECT * FROM tipo_busqueda_table")
+    fun listTipoBusqueda(): List<TipoBusqueda?>?
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addTipoDocumento(tipoDocumento: TipoDocumento): Long
+    @Query("DELETE FROM tipo_documento_table")
+    fun deleteTipoDocumento()
+    @Query("SELECT * FROM tipo_documento_table")
+    fun listTipoDocumento(): List<TipoDocumento?>?
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addTipoTransporte(tipoTransporte: TipoTransporte): Long
+    @Query("DELETE FROM tipo_transporte_table")
+    fun deleteTipoTransporte()
+    @Query("SELECT * FROM tipo_transporte_table")
+    fun listTipoTransporte(): List<TipoTransporte?>?
 }
