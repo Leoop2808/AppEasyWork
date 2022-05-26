78bcfb204bd63c6b05c620bed48d73cd1929a38c
Joaquin-Carbajal <49212899+Joaquin-Carbajal@users.noreply.github.com>
2022-05-26 01:05:10 -0500

    cambios vistas

--- app/src/main/java/com/proy/easywork/presentation/SelectLocationFragment.kt
+++ app/src/main/java/com/proy/easywork/presentation/SelectLocationFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.proy.easywork.R
+import com.proy.easywork.databinding.FragmentLoginCodePhoneFbGoogleBinding
+import com.proy.easywork.databinding.FragmentSelectLocationBinding
 
 class SelectLocationFragment : Fragment() {
 
+    private lateinit var binding: FragmentSelectLocationBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_location, container, false)
+        savedInstanceState: Bundle?): View? {
+
+        binding = FragmentSelectLocationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imgFlechaAtras.setOnClickListener {
+            Navigation.findNavController(view).popBackStack()
+        }
+
     }
 }
