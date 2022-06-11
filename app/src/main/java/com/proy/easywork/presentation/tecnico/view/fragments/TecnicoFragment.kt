c309cb34b63b06ccd2d580aa3699188447f4185a
Joaquin-Carbajal <49212899+Joaquin-Carbajal@users.noreply.github.com>
2022-06-11 04:11:16 -0500

    cambios vista principal tecnico 2

--- app/src/main/java/com/proy/easywork/presentation/tecnico/view/fragments/TecnicoFragment.kt
+++ app/src/main/java/com/proy/easywork/presentation/tecnico/view/fragments/TecnicoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
@@ -41,11 +42,21 @@ class TecnicoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
+        capturarCheckBox()
         setUpUI()
         setUpEvents()
     }
+
+    private fun capturarCheckBox() {
+        if (binding.cbFiltroCategorias.isChecked()==true){
+            Toast.makeText(context,"Filtro de categorías activado",Toast.LENGTH_SHORT).show()
+        }
+
+        if (binding.cbFiltroZonas.isChecked()==true){
+            Toast.makeText(context,"Filtro de zonas activado",Toast.LENGTH_SHORT).show()
+        }
+    }
+
     private fun setUpUI() {
         viewModel.onMessageError.observe(viewLifecycleOwner){
             it?.let {
